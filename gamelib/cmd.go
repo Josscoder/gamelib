343de664b0a8f93aa194041479f9f8c7eb93fce6
Josscoder <jomejiaar@unal.edu.co>
2026-06-20 15:22:21 -0500

    feat: impl state series commands

---
+++ gamelib/cmd.go
@@ -0,0 +1,153 @@
+package gamelib
+
+import (
+	"errors"
+
+	"github.com/df-mc/dragonfly/server/cmd"
+	"github.com/df-mc/dragonfly/server/player"
+	"github.com/df-mc/dragonfly/server/world"
+	"github.com/sandertv/gophertunnel/minecraft/text"
+)
+
+func matchFromSource(engine *Engine, src cmd.Source) (*Match, error) {
+	p, ok := src.(*player.Player)
+	if !ok {
+		return nil, errors.New("only players can execute this command")
+	}
+
+	sess, ok := engine.Session(p.XUID())
+	if !ok {
+		return nil, errors.New("you are not playing")
+	}
+
+	m := sess.Match()
+	if m == nil {
+		return nil, errors.New("you are not playing")
+	}
+
+	return m, nil
+}
+
+type PauseCommand struct {
+	engine *Engine
+}
+
+func NewPauseCommand(engine *Engine) *PauseCommand {
+	return &PauseCommand{engine: engine}
+}
+
+func (pc PauseCommand) Run(src cmd.Source, output *cmd.Output, _ *world.Tx) {
+	m, err := matchFromSource(pc.engine, src)
+	if err != nil {
+		output.Error(err.Error())
+		return
+	}
+
+	series := m.StateSeries()
+
+	if series.IsPaused() {
+		output.Error("The match is already paused")
+		return
+	}
+
+	series.SetPaused(true)
+
+	output.Print(text.Colourf(
+		"<green>The match has been successfully paused</green>",
+	))
+}
+
+type ResumeCommand struct {
+	engine *Engine
+}
+
+func NewResumeCommand(engine *Engine) *ResumeCommand {
+	return &ResumeCommand{engine: engine}
+}
+
+func (rc ResumeCommand) Run(src cmd.Source, output *cmd.Output, _ *world.Tx) {
+	m, err := matchFromSource(rc.engine, src)
+	if err != nil {
+		output.Error(err.Error())
+		return
+	}
+
+	series := m.StateSeries()
+
+	if !series.IsPaused() {
+		output.Error("The match is not paused")
+		return
+	}
+
+	series.SetPaused(false)
+
+	output.Print(text.Colourf(
+		"<green>The match has successfully resumed</green>",
+	))
+}
+
+type SkipCommand struct {
+	engine *Engine
+}
+
+func NewSkipCommand(engine *Engine) *SkipCommand {
+	return &SkipCommand{engine: engine}
+}
+
+func (sc SkipCommand) Run(src cmd.Source, output *cmd.Output, _ *world.Tx) {
+	m, err := matchFromSource(sc.engine, src)
+	if err != nil {
+		output.Error(err.Error())
+		return
+	}
+
+	m.StateSeries().Skip()
+
+	output.Print(text.Colourf(
+		"<green>The match has successfully advanced to the next state</green>",
+	))
+}
+
+type PreviousCommand struct {
+	engine *Engine
+}
+
+func NewPreviousCommand(engine *Engine) *PreviousCommand {
+	return &PreviousCommand{engine: engine}
+}
+
+func (pc PreviousCommand) Run(src cmd.Source, output *cmd.Output, _ *world.Tx) {
+	m, err := matchFromSource(pc.engine, src)
+	if err != nil {
+		output.Error(err.Error())
+		return
+	}
+
+	m.StateSeries().Previous()
+
+	output.Print(text.Colourf(
+		"<green>The match has successfully returned to the previous state</green>",
+	))
+}
+
+type RewindCommand struct {
+	engine *Engine
+}
+
+func NewRewindCommand(engine *Engine) *RewindCommand {
+	return &RewindCommand{engine: engine}
+}
+
+func (rc RewindCommand) Run(src cmd.Source, output *cmd.Output, _ *world.Tx) {
+	m, err := matchFromSource(rc.engine, src)
+	if err != nil {
+		output.Error(err.Error())
+		return
+	}
+
+	m.StateSeries().Rewind()
+
+	output.Print(text.Colourf(
+		"<green>The match has been rewound to the first state</green>",
+	))
+}
